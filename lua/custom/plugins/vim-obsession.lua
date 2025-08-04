56da4ac23f66cf6044a36a6d571f1e2dbd4a087e
Jack Anderson <jackanderson02@outlook.com>
2025-08-04 09:08:36 +0100

    feat: sessions are back?

--- lua/custom/plugins/vim-obsession.lua
+++ lua/custom/plugins/vim-obsession.lua
@@ -1,15 +1,16 @@
 --[[ return {
   -- 'tpope/vim-obsession'
 } ]]
+vim.o.sessionoptions = "blank,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 return {
--- 'rmagatti/auto-session',
--- lazy = false,
---
--- ---enables autocomplete for opts
--- ---@module "auto-session"
--- ---@type AutoSession.Config
--- opts = {
---   suppressed_dirs = { '~/', '~/projects' },
---   -- log_level = 'debug',
--- }
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { '~/', '~/projects' },
+    -- log_level = 'debug',
+  }
 }
