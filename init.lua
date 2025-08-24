d928f023c0e3993c7414e3193ae7cad70cef768d
Jack Anderson <jackanderson02@outlook.com>
2025-08-24 17:44:06 +0000

    feat: remove custom/filetype shit

--- init.lua
+++ init.lua
@@ -232,10 +232,7 @@ require('lazy').setup({
     --
     {
       import = "custom/plugins"
-    },
-    {
-      import = "custom/filetype"
-    },
+    }
     -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
     --
     -- This is often very useful to both group configuration, as well as handle
