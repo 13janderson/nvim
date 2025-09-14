49c3ea80c4b7ead16e18e2c32cb5df32aa428d86
Jack Anderson <jackanderson02@outlook.com>
2025-09-14 19:57:37 +0100

    chore: update git worktree

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -54,7 +54,7 @@ return {
     -- requires xclip for copying images from clipboard in linux
     version = "*", -- recommended, use latest release instead of latest commit
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- cond = vim.startswith(vim.fn.getcwd(), vim.fn.expand "~/vault"),
+    cond = vim.startswith(vim.fn.getcwd(), vim.fn.expand "~/vault"),
     dependencies = {
       -- Required.
       "nvim-lua/plenary.nvim",
