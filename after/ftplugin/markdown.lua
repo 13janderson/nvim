1ea611dd06dc81bca86f438d77d029ee72c1cd52
Jack Anderson <jackanderson02@outlook.com>
2025-10-01 21:36:46 +0100

    feat: fix spelling file

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,9 @@
-vim.api.nvim_set_option_value("spell", true, { scope = "local" })
-vim.api.nvim_set_option_value("spelllang", "en_gb", { scope = "local" })
+local spellfile = vim.fn.stdpath("config") .. "/spell.en-utf8.add"
+
+vim.schedule(function()
+  -- Add good word with zg
+  -- Add bad word with zw
+  vim.api.nvim_set_option_value("spellfile", spellfile, { scope = "local" })
+  vim.api.nvim_set_option_value("spell", true, { scope = "local" })
+  vim.api.nvim_set_option_value("spelllang", "en_gb", { scope = "local" })
+end)
