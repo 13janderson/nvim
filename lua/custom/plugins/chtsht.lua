9c84521478ae61ae48883c95d4c959eaaa55a46a
Jack Anderson <jackanderson02@outlook.com>
2025-11-22 22:32:57 +0000

    feat: chtsht plugin is back, not sure where it ever went ngl

--- lua/custom/plugins/chtsht.lua
+++ lua/custom/plugins/chtsht.lua
@@ -1,8 +1,11 @@
 return {
-  {
-    "13janderson/chtsht.nvim",
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-    }
-  }
+  '13janderson/chtsht.nvim',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+  },
+  -- Optional setup call to change default keymap behaviour
+  -- default is <leader>sc when nothing is passed
+  config = function()
+    require('chtsht').setup '<leader>sc'
+  end,
 }
