5342c93b7f15008f2d511a6622b4768e56697e78
Jack Anderson <jack.anderson@bjss.com>
2025-12-12 09:30:22 +0000

    latest

--- lua/custom/plugins/git-worktree.lua
+++ lua/custom/plugins/git-worktree.lua
@@ -1,6 +1,7 @@
 return {
   {
-    '13janderson/git-worktree.nvim',
+    -- '13janderson/git-worktree.nvim',
+    dir = "~/projects/plugins/git-worktree.nvim",
     version = '^2',
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   }
