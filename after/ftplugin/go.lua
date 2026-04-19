71f70cacd4da952462151f91d1e9f0efca1a8ac2
Jack Anderson <jackanderson02@outlook.com>
2026-04-19 20:17:18 +0100

    feat: go @n macro, latest

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,2 +1,3 @@
 vim.fn.setreg("p", 'yiw}kofmt.Printf()i""hpa: %s\\nla, paok_')
 vim.fn.setreg("o", 'yiwofmt.Printf()i""hpa: %s\\nla, pa_')
+vim.fn.setreg("n", 'yiwoif pa != nil{\n}')
