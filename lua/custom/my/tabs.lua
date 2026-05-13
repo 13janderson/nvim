a5325a9ffb5bd067c64af096c6796bcb1b3a655e
Jack Anderson <jackanderson02@outlook.com>
2026-05-13 23:51:36 +0100

    previous tab

--- lua/custom/my/tabs.lua
+++ lua/custom/my/tabs.lua
@@ -7,6 +7,8 @@ vim.keymap.set('n', '<M-e>', function()
   vim.cmd 'tabe'
 end)
 
+vim.keymap.set('n', '<M-p>', 'g<Tab>')
+
 -- I could be the goat
 for i = 1, 10, 1 do
   vim.keymap.set('n', string.format('<M-%d>', i), function() vim.cmd(string.format('tabn %d', i)) end)
