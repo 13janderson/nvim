02b6f4fa06ce9764eb907f9c05a94ead0a2b767b
Jack Anderson <jackanderson02@outlook.com>
2026-05-14 21:42:59 +0100

    tabs

--- lua/custom/my/tabs.lua
+++ lua/custom/my/tabs.lua
@@ -1,15 +1,15 @@
 vim.keymap.set('n', '<M-l>', 'gt')
 vim.keymap.set('n', '<M-h>', 'gT')
-vim.keymap.set('n', '<M-c>', function()
-  vim.cmd 'tabc'
-end)
-vim.keymap.set('n', '<M-e>', function()
+
+vim.keymap.set('n', '<M-n>', function()
   vim.cmd 'tabe'
 end)
 
-vim.keymap.set('n', '<M-p>', 'g<Tab>')
+vim.keymap.set('n', '<M-c>', function()
+  vim.cmd 'tabc'
+end)
 
 -- I could be the goat
 for i = 1, 10, 1 do
-  vim.keymap.set('n', string.format('<M-%d>', i), function() vim.cmd(string.format('tabn %d', i)) end)
+  vim.keymap.set('n', string.format('%dgt', i), function() vim.cmd(string.format('tabn %d', i)) end)
 end
