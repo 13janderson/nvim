940a31341b82c62faa2a0aa2705297fa19292e23
Jack Anderson <jack.anderson@bjss.com>
2026-07-21 09:32:23 +0100

    fix: persistence loading for tmp files

--- lua/custom/plugins/session.lua
+++ lua/custom/plugins/session.lua
@@ -2,4 +2,6 @@
 return {
   'folke/persistence.nvim',
   opts = {},
+  -- this gets in the way for tmp directories. e.g. when editing crontabs, editing opencode prompts.
+  cond = not vim.startswith(vim.fn.getcwd(), vim.fn.expand '/tmp'),
 }
